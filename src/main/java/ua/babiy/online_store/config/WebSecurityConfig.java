f9c39c65824297d179969345e7ec9560001c3785
Vlad <vladyslav.babiy@gmail.com>
2021-05-07 18:18:21 +0300

    added config classes

---
+++ src/main/java/ua/babiy/online_store/config/WebSecurityConfig.java
@@ -0,0 +1,56 @@
+package ua.babiy.online_store.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import ua.hubanov.onlinestore_springboot.service.impl.UserServiceImpl;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Autowired
+    private UserServiceImpl userServiceImpl;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                    .authorizeRequests()
+                    .antMatchers("/", "/tools/**", "/registration/**",
+                            "/cart/**", "/login/**")
+                    .permitAll()
+                    .antMatchers("/admin", "/admin/**").hasAnyAuthority("ADMIN")
+                    .antMatchers("/user/**").hasAnyAuthority("USER", "ADMIN")
+                    .anyRequest()
+                    .authenticated()
+                .and()
+                    .formLogin()
+                    .loginPage("/login")
+                    .defaultSuccessUrl("/user/")
+                    .permitAll()
+                .and()
+                    .logout()
+                    .logoutUrl("/j_spring_security_logout")
+                    .permitAll()
+                    .logoutSuccessUrl("/");
+    }
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+            .userDetailsService(userServiceImpl).passwordEncoder(bCryptPasswordEncoder());
+//        auth.inMemoryAuthentication().withUser("user")
+//                .password("{noop}password").roles("USER");
+    }
+
+
+}
